c8859537d0a0ddfd205d30e615e46012fe144423
NaufalZaki25 <90200851+NaufalZaki25@users.noreply.github.com>
2021-09-14 14:08:19 +0700

    Add files via upload

---
+++ I_am.java
@@ -0,0 +1,14 @@
+/*
+Naufal Zaki Zaidan
+21537144012
+*/
+public class I_am {
+    public static void main (String[] args) {
+        String name = "Naufal Zaki Zaidan";
+        int umur = 18;
+        double height = 162;
+        System.out.println ("Nama =" + name);
+        System.out.println ("umur =" + umur);
+        System.out.println ("tinggi =" + height);
+    }
+}
