fa453ac16b651421dd64177d1c8ea6aee1eefc52
NaufalZaki25 <90200851+NaufalZaki25@users.noreply.github.com>
2021-09-18 20:19:15 +0700

    Add files via upload

---
+++ Identitas.py
@@ -0,0 +1,12 @@
+Nama = "Naufal Zaki Zaidan"
+Nim = "21537144012"
+Angkatan = "2021"
+Prodi = "Teknologi Informasi"
+Jurusan = "JPTEI"
+Fakultas = "Fakultas Teknik"
+IPK = "3.50"
+
+print = ("Halo nama saya" + Nama)
+print = (Nim + "adalah NIM saya")
+print = ("saya adalah mahasiswa UNY angkatan" + Angkatan ", dengan prodi" + Prodi "jurusan" + Jurusan + Fakultas)
+print = ("Saya berharap mendapat IPK sekitar" + IPK)
